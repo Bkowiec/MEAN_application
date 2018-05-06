7265b519eac51e62a997333a39a70b9d897ce5b8
Bartosz Bukowiec <bartosz.bukowiec@synerise.com>
2018-05-06 11:55:04 +0200

    login

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,7 +12,8 @@ import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RegisterComponent } from './components/register/register.component';
 
-import { AuthService } from './services/auth.service'
+import { AuthService } from './services/auth.service';
+import { LoginComponent } from './components/login/login.component'
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { AuthService } from './services/auth.service'
     NavbarComponent,
     HomeComponent,
     DashboardComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -5,7 +5,9 @@ import 'rxjs/add/operator/map'
 @Injectable()
 export class AuthService {
 
-  domain = "http://localhost:8080"
+  domain = "http://localhost:8080";
+  authToken;
+  user;
 
   constructor(
     private http: Http
@@ -22,6 +24,17 @@ export class AuthService {
 
   checkEmail(email){
     return this.http.get(this.domain + '/authentication/checkEmail/' + email).map(res => res.json());
+  }
+
+  login(user) {
+    return this.http.post(this.domain + '/authentication/login', user).map(res => res.json());
+  }
+
+  storeUserData(token, user) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
   }
 
 }
