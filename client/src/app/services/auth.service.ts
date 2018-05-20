af10a4c7dfd92cf7821681094375d0482ecc4624
Bartosz Bukowiec <bartosz.bukowiec@synerise.com>
2018-05-20 21:41:39 +0200

    import

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -25,6 +25,25 @@ export class AuthService {
     });
   }
 
+
+
+  postClients() {
+    this.options = new RequestOptions({
+      headers: new Headers({
+        'Accept': 'application/json',
+        'Content-Type':'application/json',
+        'authorization': 'Bearer: eyJhbGciOiJSUzUxMiJ9.eyJzdWIiOiI2ZWEyZWY3MzExYjQ4MjcyNGE5YjdiMGJjMGRkODVjNiIsImF1ZCI6IkFQSSIsInJsbSI6ImJ1c2luZXNzX3Byb2ZpbGUiLCJjdGQiOjE1MjYxNzgxNDIxOTIsImlzcyI6IlN5bmVyaXNlIiwiYnBpIjo0ODAsImV4cCI6MTUyNjE4MTc0MiwiYXBrIjoiNDUwQTgzM0YtNDg4NC03Njg4LUFCQTYtRUE0N0M2M0Q4Njk0In0.MV8tj9fEUBpAA95xDPDH-ObhGZWMumfAJSj30Jb5QwY5-VogWdise7stpMi43f16KsUMG_l5gVx4QqsGiv2n103sZovuBJ7h72F1rBYkJPZgCn5sayAoba_QvEWB17k5oVyPIzDEAkIiVVt5BKIRyqBih8JS3EI1ZJjWHkCdkm2xrTFWBTmnzo1NU_KMMENc2hU7xJ7qpEIyq-5o4lfou59MmpxpqPpNDYL22Dxcq54XHXbBEwm6Pc8-uT0PB6r3PObAylt-5haYFcc76xyHRNSHY1Bl08Z4daKB2vt3hlds5MK3tMLG0t5pIILjOwpc6iH7HSzwmAyAOOpBrt2OmjRJMrkN7iX14RBvrmnRbX6nk2ObCGVg5DvOvMdX2TEsyfCa973zPLZ6KVOBYl5x2asD3LzBp2xir9mdlQrD_fvdSB5QC1YF20Ef-z33tmAPxnmG_CN-VnSLuPkGT_CVkXFjz5zpEFKgdImGQEiEaz1lphkr-fkxhoa3w39CfVd2URMvd6p-1VN_sGRcV7fq6x-tH-Z-qjHAcizd4wkeS5_h9u7WPUea059rjt9pNWi3DYPM8OM5W6bSAdeSgpdE_evU9IeOUUhJL47mRdI-5KhqS4aQF7SiZKmXKbIZ7kjvNB854kMTIMuwZ-MTGasqA-MlaMONA-nDnDjCwRIjZO8',
+        'Api-Version': '4.0'
+      })
+    });
+  }
+
+  postIt(clients){
+    this.postClients();
+    return this.http.post('https://api.synerise.com/v4/clients/batch', clients, this.options);
+  }
+
+
   loadToken() {
     this.authToken = localStorage.getItem('token');
   }
